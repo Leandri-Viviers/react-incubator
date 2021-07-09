716203a494c9f66949d8641bf7b21cf55fc1eaaa
Leandri Viviers <leandri.viviers@entelect.co.za>
2021-07-09 13:44:12 +0200

    Setup linter for jsx files and fmt stories

--- src/stories/Header.stories.jsx
+++ src/stories/Header.stories.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from 'react'
 
-import { Header } from './Header';
+import { Header } from './Header'
 
-export default {
+const ExampleHeader = {
   title: 'Example/Header',
   component: Header,
-};
+}
 
-const Template = (args) => <Header {...args} />;
+export default ExampleHeader
 
-export const LoggedIn = Template.bind({});
+const Template = (args) => <Header {...args} />
+
+export const LoggedIn = Template.bind({})
 LoggedIn.args = {
   user: {},
-};
+}
 
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {};
+export const LoggedOut = Template.bind({})
+LoggedOut.args = {}
