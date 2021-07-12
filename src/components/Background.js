3e8e1562ff8bfc11f3f14f9a5d3841e074a873d8
Leandri Viviers <leandri.viviers@entelect.co.za>
2021-07-12 12:17:50 +0200

    Background

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,9 +1,10 @@
 import { makeStyles } from '@material-ui/core'
 import BackgroundImage from '../assets/images/background.jpg'
+import Footer from './Footer'
 
 const useStyles = makeStyles({
   coverImage: {
-    height: 'calc(100vh - 48px);',
+    height: 'calc(100vh - 48px - 40px);',
     backgroundImage: `url(${BackgroundImage})`,
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
@@ -11,9 +12,23 @@ const useStyles = makeStyles({
   },
 })
 
-function Background(props) {
+export default function Background(props) {
   const classes = useStyles()
-  return <div className={classes.coverImage}>{props.children}</div>
-}
+  const attr = {
+    user: {
+      name: 'Jeff Lim',
+      link: 'https://unsplash.com/@jeffaloo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+    },
+    source: {
+      name: 'Unsplash',
+      link: 'https://unsplash.com/s/photos/dark-theme?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+    },
+  }
 
-export default Background
+  return (
+    <>
+      <div className={classes.coverImage}>{props.children}</div>
+      <Footer attr={attr}></Footer>
+    </>
+  )
+}
