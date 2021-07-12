87ce47fec0e9dcf81e1cf7d017db1c03556b0cb9
Leandri Viviers <leandri.viviers@entelect.co.za>
2021-07-12 12:16:19 +0200

    Theme spacing

--- src/theme.js
+++ src/theme.js
@@ -2,13 +2,14 @@ import { createTheme } from '@material-ui/core'
 import { grey, cyan } from '@material-ui/core/colors'
 
 const theme = createTheme({
+  spacing: 8,
   palette: {
     type: 'dark',
     primary: {
       main: grey[900],
     },
     secondary: {
-      main: cyan['A700'],
+      main: cyan['700'],
     },
   },
 })
