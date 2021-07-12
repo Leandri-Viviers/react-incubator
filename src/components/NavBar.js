b126221e2bef8db14cb7d0923029d9acfbdfc588
Leandri Viviers <leandri.viviers@entelect.co.za>
2021-07-12 12:17:37 +0200

    Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,13 @@
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
+import { Container, AppBar, Toolbar, Typography } from '@material-ui/core'
 
-const NavBar = (props) => {
+export default function Navbar(props) {
   return (
     <AppBar position="static" color="primary">
       <Toolbar variant="dense">
-        <Typography variant="body2">{props.title}</Typography>
+        <Container max-width="lg">
+          <Typography variant="body2">{props.title}</Typography>
+        </Container>
       </Toolbar>
     </AppBar>
   )
 }
-
-export default NavBar
